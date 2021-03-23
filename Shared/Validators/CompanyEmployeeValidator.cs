5aa2b238905431b26f57f40ebe27c3a48a3fd8ef
Simona Colapicchioni <s.colapicchioni@gmail.com>
2021-03-23 13:10:27 +0100

    CSS adjustments

---
+++ Shared/Validators/CompanyEmployeeValidator.cs
@@ -0,0 +1,19 @@
+﻿using CursusAdministratie2021.Shared.Models;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursusAdministratie2021.Shared.Validators {
+    public class CompanyEmployeeValidator : AbstractValidator<CompanyEmployee> {
+        public CompanyEmployeeValidator() {
+            RuleFor(companyEmployee => companyEmployee.Name).MaximumLength(200).NotNull();
+            RuleFor(companyEmployee => companyEmployee.Surname).MaximumLength(200).NotNull();
+            RuleFor(companyEmployee => companyEmployee.Company).MaximumLength(200).NotNull();
+            RuleFor(companyEmployee => companyEmployee.Department).MaximumLength(200);
+            RuleFor(companyEmployee => companyEmployee.Quotation).MaximumLength(30);
+        }
+    }
+}
---
+++ Shared/Validators/PrivateCitizenValidator.cs
@@ -0,0 +1,21 @@
+﻿using CursusAdministratie2021.Shared.Models;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursusAdministratie2021.Shared.Validators {
+    public class PrivateCitizenValidator : AbstractValidator<PrivateCitizen> {
+        public PrivateCitizenValidator() {
+            RuleFor(privateCitizen => privateCitizen.Name).MaximumLength(200).NotNull();
+            RuleFor(privateCitizen => privateCitizen.Surname).MaximumLength(200).NotNull();
+            RuleFor(privateCitizen => privateCitizen.StreetName).MaximumLength(200);
+            RuleFor(privateCitizen => privateCitizen.HouseNumber).MaximumLength(6);
+            RuleFor(privateCitizen => privateCitizen.ZipCode).MaximumLength(30);
+            RuleFor(privateCitizen => privateCitizen.City).MaximumLength(100);
+            RuleFor(privateCitizen => privateCitizen.IBAN).MaximumLength(40);
+        }
+    }
+}
